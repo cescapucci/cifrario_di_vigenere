4f6952018f36ecc21d4a9b809ebbecf1207bf01d
cescapucci <francesca.pucci@polofermigiorgi.it>
2023-12-06 18:05:58 +0100

    decifra e cifra

--- src/Cifrario.java
+++ src/Cifrario.java
@@ -9,7 +9,9 @@ public class Cifrario {
         popolaMatrice();
     }
 
-
+    /**
+     * metodo per  popolare la  matrice
+     */
     public void popolaMatrice() {
         int k = 0;
 
@@ -34,8 +36,13 @@ public class Cifrario {
  */
     }
 
-
-    public String convertiStringa(String msg, String k){
+    /**
+     * metodo che converte  dato il testo e la chiave  in input
+     * @param msg
+     * @param k
+     * @return
+     */
+    public String decifra(String msg, String k){
         String convertito ="";
 
         for (int i=0; i < msg.length(); i++){
@@ -48,22 +55,43 @@ public class Cifrario {
         return convertito;
     }
 
+    /**
+     * restituisce il carattere nella posizione i,j della matrice, il carattere in chiaro
+     * @param i
+     * @param j
+     * @return
+     */
     public char carattereConvertito(char i, char j){
         return matrice[trovai(i)][trovaj(j)];
     }
 
     //codice ascii A = &#65;
+
+    /**
+     * dalla lettera ricava l'inidce dell'array sottraendo il numero del codice ascii
+     * @param c
+     * @return
+     */
     public int trovai(char c){
         c = Character.toUpperCase(c);
         return (int)c-65;
     }
 
+    /**
+     * dalla lettera ricava l'inidce dell'array sottraendo il numero del codice ascii
+     * @param c
+     * @return
+     */
     public int trovaj(char c){
         c = Character.toUpperCase(c);
         return (int)c-65;
     }
 
-    public String convertiStringaInput(){
+    /**
+     * metodo che converte la stringa in chiaro da to testo e chiave in input
+     * @return
+     */
+    public String decifraInput(){
 
         //chiedo la stringa testo
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
@@ -73,23 +101,64 @@ public class Cifrario {
 
         //chiedo la chiave
         String k ="";
-        do {
-            System.out.println("inserire la chiave (lunga quanto  il testo)");
-            k = scanner.nextLine();  // Read user input
-            System.out.println("la  chiave è: " + k);  // Output user input
-            int aa = k.length();
-        }  while (msg.length()!=k.length());
+        System.out.println("inserire la chiave");
+        k = scanner.nextLine();  // Read user input
+        System.out.println("la  chiave è: " + k);  // Output user input
 
         String convertito ="";
 
-        for (int i=0; i < msg.length(); i++){
-            char l = msg.charAt(i); //prendo un carattere per entrmabi
-            char s = k.charAt(i);
-            char aggiungi = carattereConvertito(l,s);
-            convertito = convertito + aggiungi;
-            //System.out.println(convertito);
+        int indexK=0;
+        for (int p=0; p < msg.length(); p++){
+                char i = msg.charAt(p); //prendo un carattere per entrmabi
+                char j = k.charAt(indexK);
+                char aggiungi = carattereConvertito(i, j);
+                convertito = convertito + aggiungi;
+
+            if(indexK < k.length()-1) {indexK++;}
+                else indexK = 0;
         }
         System.out.println("il messaggio in chiaro è: " + convertito);
         return convertito;
+    }
+
+    public String cifra(String chiaro, String k){
+
+return null;
+    }
+
+    public String cifraInput(){
+
+        //chiedo la stringa in chiaro
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("inserire il testo in chiaro");
+        String msg = scanner.nextLine();  // Read user input
+        System.out.println("il testo in chiaro è: " + msg);  // Output user input
+
+        //chiedo la chiave
+        String k ="";
+
+        System.out.println("inserire la chiave");
+        k = scanner.nextLine();  // Read user input
+        System.out.println("la  chiave è: " + k);  // Output user input
+
+        String cifrato ="";
+
+        for (int i=0; i<msg.length(); i++) {
+
+            char c = msg.charAt(i);      //il carattere  dentro la  matrice
+            char indice = k.charAt(i);
+            int j = trovaj(indice);     //ho l'indice j della chiave da usare
+
+            for  (int p = 0; p < alfabeto.length; p++){
+                if (matrice[p][j] == c){
+                    cifrato = cifrato + matrice[p][j];
+                    break;
+                }
+            }
+            System.out.println("cifrato " + cifrato);
+        }
+
+        System.out.println(cifrato);
+        return cifrato;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,18 @@ public class Main {
     public static void main(String[] args) {
 
         Cifrario cifrario = new Cifrario();
-        System.out.println(cifrario.convertiStringa("ciaoguardamammacomemidiverto",
+
+        //decifra
+        System.out.println(cifrario.decifra("ciaoguardamammacomemidiverto",
                 "kaoekcujakaoekcujakaoekcujakao"));
 
-        System.out.println(cifrario.convertiStringa("aaaaaaaaa",
+        System.out.println(cifrario.decifra("aaaaaaaaa",
                 "abcdefghi"));
 
 
         //con le stringhe in input
-        cifrario.convertiStringaInput();
+        //cifrario.decifraInput();
+        cifrario.cifraInput();
+
     }
 }
